b464576741031d3f041c5596fcfcd36a12d944b4
erichiroshi <erichiroshi@gmail.com>
2023-07-21 10:34:20 -0400

    Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -1,19 +1,16 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.enums.ResourceType;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -27,25 +24,21 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(of = "id")
 @Entity
-@Table(name = "tb_offer")
-public class Offer implements Serializable {
+@Table(name = "tb_resource")
+public class Resource implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String edition;
-
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private LocalDateTime startMoment;
-
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private LocalDateTime endMoment;
+	private String title;
+	private String description;
+	private Integer position;
+	private String imgUri;
+	private ResourceType type;
 
 	@ManyToOne
-	@JoinColumn(name = "course_id")
-	private Course course;
+	@JoinColumn(name = "offer_id")
+	private Offer offer;
 
-	@OneToMany(mappedBy = "offer")
-	private final List<Resource> resources = new ArrayList<>();
 }
